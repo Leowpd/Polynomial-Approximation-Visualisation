196cd757ef173d545b08066dd8f7da9298f2e4c0
Leo Duncan <leo.wouter.duncan@gmail.com>
2024-07-04 20:27:21 +1200

    Aesthetic inprovements, more detailed comments, more user-friendly

--- version_3.m
+++ version_3.m
@@ -1,52 +1,78 @@
 %version 3 is hopefully gonna combine versions 1 and 2 so you can see both
 %animations simultaneously.
 
+syms function_to_approximate(x)
+
+%%%%%%%%%%%%%%%% USER INPUT %%%%%%%%%%%%%%%% sin(x)*log(exp(x))/3
+
+function_to_approximate(x) = sin(x);
+max_polynomial_degree = 13;
+
+%input "0" for the Maclaurin polynomial 
+taylor_approximate_around_point = 0;
+
+%speed in seconds of animation
+speed = 0.5;
+
+%viewing window
+minX = -30;
+maxX = 30;
+minY = -10;
+maxY = 10;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 
 syms f(x)
-f(x) = sin(x); %could be any (simple?) function we want to approximate.
+f(x) = function_to_approximate; %our function to approximate
 
-minX = -20;
-maxX = 20;
-minY = -7;
-maxY = 7;
-max_degree = 13;
+x0 = linspace(minX,maxX,1000);
 
+primary_line_weight = 4;
+secondary_line_weight = 2;
 
-
-x0 = linspace(minX,maxX); %change to make more flexible in future iterations
 
 figure(1)
+t = tiledlayout(2,1);
+title(t, strcat({'Degree '}, num2str(max_polynomial_degree), ' Taylor Polynomial of f(x) = ', string(function_to_approximate(x))))
 
 % Tile 1
 nexttile(1);
 hold on
 grid on
-plot(x0,f(x0), 'LineWidth',1.5, 'DisplayName','f(x)')
+plot(x0,f(x0), 'black', 'LineWidth',primary_line_weight, 'DisplayName','f(x)')
+title('nth Taylor Polynomial')
+line1 = plot(0,0,'visible','off', 'HandleVisibility','off'); %initialises "line1" so that "delete(line1);" later on has no issues.
 
 xlim([minX maxX])
 ylim([minY maxY])
+
+legend
 
 
 % Tile 2
 nexttile(2);
 hold on
 grid on
-plot(x0,f(x0), 'LineWidth',1.5, 'DisplayName','f(x)')
+plot(x0,f(x0), 'black', 'LineWidth',primary_line_weight, 'DisplayName','f(x)')
+title('First n Taylor Polynomials')
+line2 = plot(0,0,'visible','off', 'HandleVisibility','off'); %ensures the colours of both graphs are consistent.
 
 xlim([minX maxX])
 ylim([minY maxY])
 
+legend
+
 
 %series calculation starts here
 syms p_k(x)
-a = 0;
-line1 = plot(0,0,'visible','off');
+a = taylor_approximate_around_point;
 
 p_k(x) = f(a);
-Df = f;
+Df = f; %initialise derivative to 0th derivative
 
-for k = 1:max_degree
+for k = 1:max_polynomial_degree
 
     p_prev = p_k;
 
@@ -54,20 +80,19 @@ for k = 1:max_degree
     p_k = p_k + Df(a)/factorial(k) * (x-a)^k; %update the kth taylor polynomial
 
     if p_k ~= p_prev
-        pause(1)
+        pause(speed) %animation speed
 
         nexttile(1)
-        delete(line1);
-        line1 = plot(x0, p_k(x0), 'LineWidth',1.5, 'DisplayName',strcat('deg.', num2str(k)));
+        delete(line1); %delete previous line
+        line1 = plot(x0, p_k(x0), 'LineWidth',primary_line_weight, 'DisplayName',strcat('deg.', num2str(k)));
         legend %update legend
 
         nexttile(2)
-        plot(x0, p_k(x0), 'DisplayName',strcat('deg.', num2str(k)))
+        plot(x0, p_k(x0), 'LineWidth',secondary_line_weight, 'DisplayName',strcat('deg.', num2str(k)))
         legend %update legend
 
         
     end
 
 end
-
 
