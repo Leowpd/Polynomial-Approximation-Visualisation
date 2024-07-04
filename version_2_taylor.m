27897ea465a748dfa05d801aff8da146a0b89ee9
Leo Duncan <leo.wouter.duncan@gmail.com>
2024-07-04 19:00:29 +1200

    Version 3 successfully combined the tiles of versions 1 & 2 so both can be viewed simultaneously.

--- version_2_taylor.m
+++ version_2_taylor.m
@@ -21,21 +21,20 @@ a = 0;
 
 p_k(x) = f(a);
 Df = f;
-line1 = plot(0,0);
+line1 = plot(0,0,'visible','off');
 for k = 1:13
 
     p_prev = p_k;
 
     Df = diff(Df,x); %get the next derivitive of f
-    p_k1 = p_prev + Df(a)/factorial(k) * (x-a)^k; %update the kth taylor polynomial
+    p_k = p_prev + Df(a)/factorial(k) * (x-a)^k; %update the kth taylor polynomial
 
-    if p_k1 ~= p_prev
+    if p_k ~= p_prev
         pause(1)
         delete(line1);
-        line1 = plot(x0, p_k1(x0), 'DisplayName',strcat('deg.', num2str(k)));
+        line1 = plot(x0, p_k(x0), 'DisplayName',strcat('deg.', num2str(k)));
         legend %update legend
     end
-    p_k = p_k1;
 
 end
 
---
+++ version_3.m
@@ -0,0 +1,73 @@
+%version 3 is hopefully gonna combine versions 1 and 2 so you can see both
+%animations simultaneously.
+
+
+
+syms f(x)
+f(x) = exp(x); %could be any (simple?) function we want to approximate.
+
+minX = -20;
+maxX = 20;
+minY = -7;
+maxY = 7;
+max_degree = 13;
+
+
+
+x0 = linspace(minX,maxX); %change to make more flexible in future iterations
+
+figure(1)
+
+% Tile 1
+nexttile(1);
+hold on
+grid on
+plot(x0,f(x0), 'LineWidth',1.5, 'DisplayName','f(x)')
+
+xlim([minX maxX])
+ylim([minY maxY])
+
+
+% Tile 2
+nexttile(2);
+hold on
+grid on
+plot(x0,f(x0), 'LineWidth',1.5, 'DisplayName','f(x)')
+
+xlim([minX maxX])
+ylim([minY maxY])
+
+
+%series calculation starts here
+syms p_k(x)
+a = 0;
+line1 = plot(0,0,'visible','off');
+
+p_k(x) = f(a);
+Df = f;
+
+for k = 1:max_degree
+
+    p_prev = p_k;
+
+    Df = diff(Df,x); %get the next derivitive of f
+    p_k = p_k + Df(a)/factorial(k) * (x-a)^k; %update the kth taylor polynomial
+
+    if p_k ~= p_prev
+        pause(1)
+
+        nexttile(1)
+        delete(line1);
+        line1 = plot(x0, p_k(x0), 'DisplayName',strcat('deg.', num2str(k)));
+        legend %update legend
+
+        nexttile(2)
+        plot(x0, p_k(x0), 'DisplayName',strcat('deg.', num2str(k)))
+        legend %update legend
+
+        
+    end
+
+end
+
+
